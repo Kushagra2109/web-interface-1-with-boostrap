5da98ec81aa3598a44f96009ddf5985bf0823873
Kushagra <20it144@charusat.edu.in>
2024-05-31 22:28:57 +0530

    same web with bootstrap

---
+++ jsintro.js
@@ -0,0 +1,12 @@
+// function secondjs(){
+//     document.getElementById('secondjs').innerHTML="this is the new text";
+//     document.getElementById('secondjs').style.backgroundColor="red";
+//     document.getElementById('secondjs').style.fontFamily="audiowide";
+//     document.getElementById('secondjs').style.fontSize="50px";
+// }
+
+// function third(){
+//     var pic;
+//     pic = "pexels-pixabay-35967.jpg"
+//     document.getElementById('thirdjs').src= pic;
+// }
